00212d4d0c0a5937dee07d9f13ce61868a101dea
feron <avel.feron@gmail.com>
2020-05-06 09:21:11 +0200

    maj

--- src/shared/config.js
+++ src/shared/config.js
@@ -2,5 +2,5 @@
 
 export const config = {
   API_URL : 'http://localhost:8080/apiPactNG/api/v1/',
-  //API_URL : 'http://localhost:8080/apiPactNG/api/v1/',
+  //API_URL : 'http://77.196.241.97:8080/apiPactNG/api/v1/',
 }
