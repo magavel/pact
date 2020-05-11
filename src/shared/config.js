412ac039f500b10d24fb3b854ad31822ba0f6af1
o2020053 <yohan.agogue@etu.univ-orleans.fr>
2020-05-11 13:41:29 +0200

    correction config

--- src/shared/config.js
+++ src/shared/config.js
@@ -3,6 +3,6 @@ export const config = {
   API_AUTH : 'http://localhost:8080/apiPactNG/api/auth/',
   API_MANAGE : 'http://localhost:8080/apiPactNG/management/',
   //API_URL : 'http://77.196.241.97:8080/pactng-backend/api/v1/',
-  //API_AUTH : 'http://77.196.241.97:8080/pactng-backend/auth/',
+  //API_AUTH : 'http://77.196.241.97:8080/pactng-backend/api/auth/',
   //API_MANAGE : 'http://77.196.241.97:8080/pactng-backend/management/',
 }
