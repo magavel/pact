17cd62ac96781e150ee03cb79198149d5d3f2014
feron <avel.feron@gmail.com>
2020-06-08 10:58:15 +0200

    ajout utils

--- src/shared/utils.js
+++ src/shared/utils.js
@@ -1,18 +1,12 @@
-import moment from "moment";
-
 export default {
-    dateBetween: function(startDate, endDate) {
-        console.log('dates ds utils',startDate, endDate)
+    dateBetween(startDate, endDate) {
         let currentDate = startDate;
         let dates = [];
         endDate = new Date(endDate);
-        console.log('dates ds utils',startDate, endDate)
         while (currentDate <= endDate) {
             dates.push(currentDate);
             currentDate = new Date(currentDate);
-
             currentDate.setDate(currentDate.getDate()+1)
-
         }
         return dates;
     },
