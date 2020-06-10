55c552e2c1ac15502b53357742f994ad835fa2cb
TSEF ROGER Xavier <xrog2005@icloud.com>
2020-06-10 15:19:20 +0200

    Update config.js Suppression du doublon http://http://

--- src/shared/config.js
+++ src/shared/config.js
@@ -5,8 +5,8 @@ export const config = {
   // API_URL : 'http://77.196.241.97:8080/pactng-backend/api/v1/',
  // API_AUTH : 'http://77.196.241.97:8080/pactng-backend/api/auth/',
 //  API_MANAGE : 'http://77.196.241.97:8080/pactng-backend/management/',
-    API_URL : 'http://http://pactng.concept-toulon.dirisi.defense.gouv.fr/pactng-backend/api/v1/',
-    API_AUTH : 'http://http://pactng.concept-toulon.dirisi.defense.gouv.fr/pactng-backend/api/auth/',
-    API_MANAGE : 'http://http://pactng.concept-toulon.dirisi.defense.gouv.fr/pactng-backend/management/',
+    API_URL : 'http://pactng.concept-toulon.dirisi.defense.gouv.fr/pactng-backend/api/v1/',
+    API_AUTH : 'http://pactng.concept-toulon.dirisi.defense.gouv.fr/pactng-backend/api/auth/',
+    API_MANAGE : 'http://pactng.concept-toulon.dirisi.defense.gouv.fr/pactng-backend/management/',
 }
 
