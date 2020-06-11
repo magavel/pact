51797878d1fe0d4a6362cef04aa53cfceaef5ab0
TSEF ROGER Xavier <xrog2005@icloud.com>
2020-06-11 11:21:35 +0200

    Update config.js

--- src/shared/config.js
+++ src/shared/config.js
@@ -5,8 +5,8 @@ export const config = {
   // API_URL : 'http://77.196.241.97:8080/pactng-backend/api/v1/',
  // API_AUTH : 'http://77.196.241.97:8080/pactng-backend/api/auth/',
 //  API_MANAGE : 'http://77.196.241.97:8080/pactng-backend/management/',
-    API_URL : 'http://pactng-api.concept-toulon.dirisi.defense.gouv.fr/pactng-backend/api/v1/',
-    API_AUTH : 'http://pactng-api.concept-toulon.dirisi.defense.gouv.fr/pactng-backend/api/auth/',
-    API_MANAGE : 'http://pactng-api.concept-toulon.dirisi.defense.gouv.fr/pactng-backend/management/',
+    API_URL : 'https://pactng-api.concept-toulon.dirisi.defense.gouv.fr/pactng-backend/api/v1/',
+    API_AUTH : 'https://pactng-api.concept-toulon.dirisi.defense.gouv.fr/pactng-backend/api/auth/',
+    API_MANAGE : 'https://pactng-api.concept-toulon.dirisi.defense.gouv.fr/pactng-backend/management/',
 }
 
