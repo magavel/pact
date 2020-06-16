9970996eb403cc1fe7c6872ade567339b547d0a0
feron <avel.feron@gmail.com>
2020-06-16 16:20:14 +0200

    ajout card ds layout

---
+++ src/components/CentralContainer.vue
@@ -0,0 +1,84 @@
+<template>
+    <div class="container-fluid">
+        <div class="row ">
+            <nav class=" navGauche col-md-2 d-none d-md-block">
+                <div class="">
+                    <ul class="nav flex-column">
+                        <li class="nav-item">
+                            <router-link v-if="currentUser" to="/home" class="nav-link">
+                                <font-awesome-icon icon="chalkboard"/>
+                                Rapport
+                            </router-link>
+                        </li>
+                        <li class="nav-item">
+                            <router-link v-if="currentUser" to="/activites" class="nav-link">
+                                <font-awesome-icon icon="pencil-alt"/>
+                                saisie
+                            </router-link>
+                        </li>
+                        <li class="nav-item">
+                            <router-link v-if="currentUser" to="/projects" class="nav-link">
+                                <font-awesome-icon icon="pencil-alt"/>
+                                Projets
+                            </router-link>
+                        </li>
+                        <li v-if="showAdminBoard" class="nav-item">
+                            <router-link to="/user" class="nav-link">
+                                <font-awesome-icon icon="cogs"/>
+                                Administration
+                            </router-link>
+                        </li>
+                        <li v-if="showModeratorBoard" class="nav-item">
+                            <router-link to="/mod" class="nav-link"> Board Pilote</router-link>
+                        </li>
+                    </ul>
+
+
+                </div>
+            </nav>
+
+
+            <main role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
+                <router-view/>
+            </main>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'NavigationGauche',
+        props: {
+            currentUser: {},
+            showAdminBoard: {},
+            showModeratorBoard: {}
+        }
+    }
+</script>
+<style lang="scss" scoped>
+    @import "../assets/custom";
+    @import "../../node_modules/bootstrap/scss/bootstrap";
+    .navGauche {
+
+    }
+    body {
+        background-color: $background;
+
+        & a {
+            font-weight: bold;
+            height: 50px;
+            margin-right: -15px;
+            color: white;
+
+            &:hover {
+                color: $secondary;
+            }
+        }
+
+    }
+
+    .dropdownWidth textarea {
+        width: 90%;
+    }
+
+
+</style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,79 @@
+<template>
+    <nav class="navbar  navbar-expand-lg navbar-dark sticky-top flex-md-nowrap p-0">
+        <div v-if="!currentUser" class="navbar-nav ml-auto">
+            <ul class="navbar-nav px-3 ">
+                <li class="nav-item text-primary">
+                    <router-link to="/login" class="nav-link">
+                        <font-awesome-icon icon="sign-in-alt"/>
+                        Connexion
+                    </router-link>
+                </li>
+            </ul>
+        </div>
+        <div v-if="currentUser" class="navbar-nav ml-auto">
+            <ul class="navbar-nav px-3">
+                <li class="nav-item">
+                    <div class="dropdown">
+                        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <font-awesome-icon icon="user"/>
+                            {{ currentUser.username }}
+                        </button>
+                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+
+                            <router-link to="/profile" class="dropdown-item">
+                                <font-awesome-icon icon="user"/>
+                                Profile
+                            </router-link>
+                            <router-link to="/updatePassword" class="dropdown-item">
+                                <font-awesome-icon icon="user"/>
+                                Changer son mot de passe
+                            </router-link>
+
+                            <a class="dropdown-item" href @click.prevent="logOut">
+                                <font-awesome-icon icon="sign-out-alt"/>
+                                Deconnexion
+                            </a>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</template>
+<script>
+    export default {
+        name: 'Navigation',
+        props: {
+            currentUser: {},
+            logOut: {}
+        }
+    }
+</script>
+<style lang="scss">
+    @import "../assets/custom";
+    @import "../../node_modules/bootstrap/scss/bootstrap";
+
+    body {
+        background-color: $background;
+
+        & a {
+            font-weight: bold;
+            height: 50px;
+            margin-right: -15px;
+            color: white;
+
+            &:hover {
+                color: $secondary;
+            }
+        }
+
+    }
+
+
+    .dropdownWidth textarea {
+        width: 90%;
+    }
+
+
+</style>
