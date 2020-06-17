5f37c4b96be09264fb5ecb2a1484439bd0b6b4cc
feron <avel.feron@gmail.com>
2020-06-17 14:52:42 +0200

    maj css layout

--- src/filters/jourFrShort.js
+++ src/filters/jourFrShort.js
@@ -1,5 +1,11 @@
 import moment from 'moment';
 
+/**
+ * return le jours en trois lettres;
+ * ex: mer pour mercredi
+ * @param value
+ * @returns {string}
+ */
 export default function jourFrShort(value) {
   return moment(value).format('dd');
 }
--- src/filters/monthYearShort.js
+++ src/filters/monthYearShort.js
@@ -1,5 +1,11 @@
 import moment from 'moment';
 
+/**
+ * return le mois et l'année sous la forme:
+ * 12/20
+ * @param value
+ * @returns {string}
+ */
 export default function monthYearShort(value) {
   return moment(value).format('DD/MM');
 }
